987d20bca4536ce3b3b17fa007a85ba11024d127
Nanichang Katzing <nanipaul68@gmail.com>
2023-03-23 16:36:59 -0700

    added a fix to 2-int_index.c file

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,4 +1,6 @@
 #include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
 /**
  * int_index - return index place if comparison = true, else -1
  * @array: array
@@ -10,12 +12,17 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (array == NULL || size <= 0 || cmp == NULL)
+	if (array == NULL)
 		return (-1);
+	if (size <= 0)
+		return (-1);
+	if (cmp == NULL)
+		return (-1);
+
 	for (i = 0; i < size; i++)
 	{
-		if (cmp(array[i]))
+		if (cmp(array[i]) != 0)
 			return (i);
 	}
-	return (-1)
+	return (-1);
 }
